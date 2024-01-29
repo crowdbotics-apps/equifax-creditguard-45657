fc89662d40d5e9f961ce61cd28c4c27c306f881c
Crowdbotics <team@crowdbotics.com>
2024-01-29 05:16:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "equifax-creditguard-45657",
+  url: "https://equifax-creditguard-45657.botics.co",
+  api: "https://equifax-creditguard-45657.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
